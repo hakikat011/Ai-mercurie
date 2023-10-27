e0c255c64ba72345fd84a9a9c46b5eb21e17a5ae
hakikat011 <114380967+hakikat011@users.noreply.github.com>
2023-10-27 15:33:29 -0700

    Update Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,8 +6,8 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
-      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+      <h1 className="gradient__text">OPEN AI - ChatGPT is a gamechanger in the field of LLMs </h1>
+      <p>ChatGPT is a powerful language model that has the potential to revolutionize the way we interact with and utilize artificial intelligence in our daily lives.</p>
 
       <div className="gpt3__header-content__input">
         <input type="email" placeholder="Your Email Address" />
